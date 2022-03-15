714a28f40ed138b21a8f473f8719304a638582c4
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2022-03-15 16:19:52 +0300

    Сделал разметку

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
 $(() => {
 
+    $('.reviews_items').slick({
+        infinite: true,
+        dots: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+            }
+        }, {
+            breakpoint: 875,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
 
 
 })
