cc38aa02aebe0d693f10b2db39d4571792917322
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2022-03-16 16:18:41 +0300

    Правки2

--- js/scripts.js
+++ js/scripts.js
@@ -8,23 +8,29 @@ $(() => {
         slidesToShow: 2,
         slidesToScroll: 1,
         responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        }, {
-            breakpoint: 875,
+            breakpoint: 1023,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows: false
+            }
+        }, {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false                
             }
         }, {
             breakpoint: 480,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false
             }
         }]
     });
